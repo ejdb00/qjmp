22a180826a2cea8c636b831a75fbef257f9a7d8b
Eli Berg <ejdb00@gmail.com>
2016-05-25 22:39:55 -0700

    finished first draft of experiment script and hadoop simulator

--- exp1.py
+++ exp1.py
@@ -6,6 +6,14 @@ from mininet.link import TCLink
 from mininet.net import Mininet
 from mininet.util import dumpNodeConnections
 from mininet.cli import CLI
+import hadoop_sim
+import os
+import signal
+
+ptpdClientProcess = {}
+ptpdServerProcess = {}
+memcachedClientProcess = {}
+memcachedServerProcess = {}
 
 class QJmpTopo(Topo):
   "Topology for QJump experiments"
@@ -33,13 +41,93 @@ class QJmpTopo(Topo):
 
     return
 
-def startPTPd():
-  server_cmd = "ptpd -M -c -b h1-eth0 -y 0 -D -h -T 10"
+def startPTPd(net, outfile, priority):
+  ptpdServer = net.getNodeByName("h8")
+  ptpdClient = net.getNodeByName("h1")
+  server_cmd = './qjau.py -p %d -c \"ptpd -M -c -b h8-eth0 -y 0 -D -h -T 10\"' % priority
+  client_cmd = './qjau.py -p %d -c \"ptpd -x -c -g -D -b h1-eth0 -h -T 10 -f %s\"' % (priority, outfile)
+  ptpdServerProcess = ptpdServer.popen(server_cmd, stdout=subprocess.PIPE, shell=True, preexec_fn=os.setsid)
+  ptpdClientProcess = ptpdClient.popen(client_cmd, stdout=subprocess.PIPE, shell=True, preexec_fn=os.setsid)
+
+def startMemcached(net, outfile, priority):
+  memcachedServer = net.getNodeByName("h11")
+  memcachedClient = net.getNodeByName("h3")
+  server_cmd = './qjau.py -p %d -c \"/usr/bin/memcached -m 64 -p 11211 -u memcache\"' % priority
+  client_cmd = './qjau.py -p %d -c \"../clients/memaslap -s %s:11211 -S 1s -B -T2 -c 3 -X 128 > %s\"' % (priority, memcachedServer.IP(), outfile)
+  memcachedServerProcess = memcachedServer.popen(server_cmd, stdout=subprocess.PIPE, shell=True, preexec_fn=os.setsid)
+  memcachedClientProcess = memcachedClient.popen(client_cmd, stdout=subprocess.PIPE, shell=True, preexec_fn=os.setsid)
+
+def stopPTPd():
+  os.killpg(os.getpgid(ptpdClientProcess.pid), signal.SIGTERM)
+  os.killpg(os.getpgid(ptpdServerProcess.pid), signal.SIGTERM)
+
+def stopMemcached():
+  os.killpg(os.getpgid(memcachedClientProcess.pid), signal.SIGTERM)
+  os.killpg(os.getpgid(memcachedServerProcess.pid), signal.SIGTERM)
+
+def runExp1(net):
+  startPTPd(net, "./data/exp1_PTPd_out", 0)
+  startMemcached(net, "./data/exp1_memcached_out", 0)
+  timer.sleep(10 * 60)
+  stopPTPd()
+  stopMemcached()
+
+def runExp2(net, hadoop):
+  startPTPd(net, "./data/exp2_PTPd_out", 0)
+  startMemcached(net, "./data/exp2_memcached_out", 0)
+  hadoop.runHadoopSimulation()
+  stopPTPd()
+  stopMemcached()
+
+def runExp3(net, hadoop):
+  startPTPd(net, "./data/exp3_PTPd_out", 7)
+  startMemcached(net, "./data/exp3_memcached_out", 5)
+  hadoop.runHadoopSimulation()
+  stopPTPd()
+  stopMemcached()
+
+def configureHadoopSim(net, hadoop_dir):
+  hostnames = []
+  for i in range(12):
+    hostname = "h%d" % i + 1
+    hostnames.append(hostname)
+  workernames = ["h4", "h5", "h6", "h7", "h9", "h10", "h12"]
+  workers = []
+  for wn in workernames:
+    workers.append(net.getNodeByName(wn))
+  master = net.getNodeByName("h2")
+  hadoop = hadoop_sim.HadoopSim(hostnames, master, workers, hadoop_dir, sizes, 0)
+  return hadoop
+
+def plotData():
+  cmd = "python plot_ptp_memcached_hadoop_timeline.py \
+      ./data/exp1_PTPd_out \
+      ./data/exp1_memcached_out \
+      ./data/exp2_PTPd_out \
+      ./data/exp2_memcached_out \
+      ./data/exp3_PTPd_out \
+      ./data/exp3_memcached_out"
+  os.system(cmd)
 
 def main():
   topo = QJmpTopo()
   net = Mininet(topo=topo, host=CPULimitedHost, link = TCLink)
+  hadoopDir = "/tmpfs/hadoop/"
+  hadoop = configureHadoopSim(net, hadoopDir)
+  hadoop.generateFiles()
 
+  net.start()
+
+  runExp1(net)
+
+  #runExp2(net)
+
+  #runExp3(net)
+
+  net.stop()
+  hadoop.removeFiles()
+
+  #plotData()
 
 if __name__ == "__main__":
   main()
--- hadoop_sim.py
+++ hadoop_sim.py
@@ -2,49 +2,92 @@ import os
 import random
 import timer
 import shutil
+import sets
 
-def generate_files(hostnames, dir_path, sizes):
-  for hn in hostnames:
-    for sz in sizes:
-      filename = hn + str(sz)
-      full_path = os.path.join(dir_path, filename)
-      f = open(full_path, 'br+')
-      for b in range(sz/16):
-        f.write(os.urandom(16))
-      f.close()
+class HadoopSim:
+
+  mb = 1024 * 1024
+  kb = 1024
+
+  def __init__(self, hostnames, master, workers, dirPath, sizes, priority):
+    self.hostnames = hostnames
+    self.dirPath = dirPath
+    self.sizes = sizes
+    self.master = master
+    self.workers = workers
+    self.priority = priority
+
+  def generateFiles(self):
+    for hn in self.hostnames:
+      for sz in self.sizes:
+        filename = hn + str(sz)
+        full_path = os.path.join(self.dir_path, filename)
+        f = open(full_path, 'br+')
+        for b in range(sz/16):
+          f.write(os.urandom(16))
+        f.close()
+
+  def sendFileOfSize(self, sender, receiver, size):
+    r_name = receiver.name
+    s_name = sender.name
+    filename = os.path.join(self.dirPath, s_name + str(size))
+
+    port = random.randint(1024, 65535)
+
+    recCmd = 'nc -l %d' % port
+    sendCmd = './qjau.py -p %d -c \"nc %s %d < %s\"' % (self.priority, receiver.IP(), port, filename)
+
+    receiver.popen(recCmd, shell=True)
+    sender.popen(sendCmd, shell=True)
 
 
+  def removeFiles(self):
+    shutil.rmtree(self.dirPath, ignore_errors=True)
 
-def send_file_of_size(sender, receiver, size, dir_path):
-  r_name = receiver.name
-  s_name = sender.name
-  filename = os.path.join(dir_path, s_name + str(size))
+  def generateShuffleSets(self):
+    self.workersSet = sets.Set(range(len(self.workers)))
+    self.shuffleSets = []
+    for w in range(len(self.workers)):
+      self.shuffleSets.append(sets.Set(range(len(self.workers))))
+      self.shuffleSets[w].remove(w)
 
-  port = random.randint(1024, 65535)
+  def runDistribution(self):
+    for n in range(self.replicationFactor):
+      order = range(len(self.workers))
+      random.shuffle(order)
+      for i in order:
+        sendFileOfSize(self.master, self.workers[i], 5 * self.mb)
+        timer.sleep(random.random() / 4.0)
 
-  rec_cmd = 'nc -l %d' % port
-  receiver.popen(rec_cmd, shell=True)
+  def runShuffle(self):
+    nWorkers = len(self.workers)
+    while len(self.workersSet) > 0:
+      rs = random.randint(0, nWorkers - 1)
+      if rs in self.workersSet:
+        sender = workers[rs]
+        rr = random.randint(0, nWorkers - 1)
+        if rr in self.shuffleSets[rs]:
+          receiver = workers[rr]
+          sendFileOfSize(sender, receiver, 10 * self.mb)
+          self.shuffleSets[rs].remove(rr)
+          if len(self.shuffleSets[rs]) == 0:
+            self.workersSet.remove(rs)
+          timer.sleep(random.random() / 2.0)
 
-  send_cmd = 'nc %s %s < %s' % (receiver.IP(), port, filename)
-  sender.popen(send_cmp, shell=True)
+  def runCollection(self):
+    for n in range(self.replicationFactor):
+      order = range(len(self.workers))
+      random.shuffle(order)
+      for i in order:
+        sendFileOfSize(self.workers[i], self.master, 10 * self.mb)
+        timer.sleep(random.random() * 2)
 
+  def runHadoopSimulation(self):
+    self.generateShuffleSets()
+    self.runDistribution()
+    timer.sleep(20)
+    self.runShuffle()
+    timer.sleep(5)
+    self.runCollection()
 
-def remove_files(dir_path):
-  shutil.rmtree(dir_path, ignore_errors=True)
-
-def run_distribution(master, workers, dir_path):
-  for w in workers:
-    send_file_of_size(master, workers[w], 50 * 1024 * 1024)
-
-def run_mappers_to_reducers(master, workers, dir_path):
-
-
-def run_reducers_to_master(master, workers, dir_path):
-
-
-def run_hadoop_emulation(master, workers, dir_path):
-  run_distribution(master, workers)
-  timer.sleep(10)
-  run_mappers_to_reducers(master, workers)
-  run_reducers_to_master(master, workers)
 
